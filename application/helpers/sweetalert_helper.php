b934dd7c7736f00c36660a6b43e4b4f692042363
chandanjaiswallove <chandanjaiswalloves@gmail.com>
2026-03-18 07:49:05 +0530

    sweet alert 2 function add in helper but not update

--- application/helpers/sweetalert_helper.php
+++ application/helpers/sweetalert_helper.php
@@ -17,3 +17,52 @@ function sweetAlert($title, $text, $icon, $redirect)
     </script>";
     exit;
 }
+
+
+// function sweetAlert($title, $text, $icon, $redirect)
+// {
+//     echo '<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>';
+
+//     echo "<style>
+
+//     /* SweetAlert popup बड़ा */
+//     .swal-modal{
+//         width: 95% !important;
+//         max-width: 500px;
+//         padding: 30px;
+//     }
+
+//     /* Title बड़ा */
+//     .swal-title{
+//         font-size: 28px !important;
+//         font-weight: bold;
+//     }
+
+//     /* Text बड़ा */
+//     .swal-text{
+//         font-size: 20px !important;
+//         line-height: 1.6;
+//     }
+
+//     /* Button बड़ा */
+//     .swal-button{
+//         font-size: 18px !important;
+//         padding: 10px 25px;
+//     }
+
+//     </style>";
+
+//     echo "<script>
+//         document.addEventListener('DOMContentLoaded', function () {
+//             swal({
+//                 title: '$title',
+//                 text: '$text',
+//                 icon: '$icon',
+//                 button: 'OK'
+//             }).then(function () {
+//                 window.location.href = '$redirect';
+//             });
+//         });
+//     </script>";
+//     exit;
+// }
